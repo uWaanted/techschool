aedaeca8804e898c1be6fa24167ec6a257af6285
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-10-10 23:54:17 +0000

    fix: Base

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const kaushan = Inter ({ 
+    subsets: ['latin'],
+    weight: '400'
+})
+
 export default function Home() {
   return (
     <h1>ESCOLA TECNOLÓGICA</h1>
